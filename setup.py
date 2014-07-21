ceb5f375f031b9629822ea8ac26b11ed797df621
Florian PAQUET <f.paquet@jurismarches.com>
2014-07-21 11:33:43 +0200

    Add moar classifiers

--- setup.py
+++ setup.py
@@ -24,7 +24,12 @@ setup(
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
     ],
     test_suite='tree_extractor.tests'
 )

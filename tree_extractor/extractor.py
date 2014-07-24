55fe58597b3e4f306d5ed6fa28127397f211bf71
Florian PAQUET <f.paquet@jurismarches.com>
2014-07-24 11:04:46 +0200

    More docs and method splits

--- tree_extractor/extractor.py
+++ tree_extractor/extractor.py
@@ -1,11 +1,12 @@
 # -*- coding:utf-8 -*-
 import re
 import cssselect
-from six.moves.urllib.parse import urljoin
-from tinycss.css21 import RuleSet, ImportRule, MediaRule, PageRule
+from itertools import chain
 from lxml import html
 from lxml.etree import strip_attributes
-from itertools import chain
+from six import string_types
+from six.moves.urllib.parse import urljoin
+from tinycss.css21 import RuleSet, ImportRule, MediaRule, PageRule
 
 from .parser import CSSParser
 from .rules import FontFaceRule
@@ -31,12 +32,11 @@ class TreeExtractor(object):
         re.IGNORECASE | re.MULTILINE)
 
     def __init__(self):
+        # Keep Xpaths expressions
+        self._xpaths_to_keep = []
 
-        # xpaths expressions to keep
-        self.xpaths_to_keep = []
-
-        # xpaths expressions to discard
-        self.xpaths_to_discard = []
+        # Discard Xpaths expressions
+        self._xpaths_to_discard = []
 
     ##########
     # Public #
@@ -44,34 +44,56 @@ class TreeExtractor(object):
 
     def keep(self, xpath):
         """
-        Adds a keep xpath expression
+        Adds a keep Xpath expression
+
+        :param e: The Xpath expression to keep
+        :type e: str
+
+        :retuns: self
+        :rtype: TreeExtractor
         """
-        self.xpaths_to_keep.append(xpath)
+        self.__add(self._xpaths_to_keep, xpath)
         return self
 
     def discard(self, xpath):
         """
-        Adds a discard xpath expression
+        Adds a discard Xpath expression
+
+        :param e: The Xpath expression to discard
+        :type e: str
+
+        :retuns: self
+        :rtype: TreeExtractor
         """
-        self.xpaths_to_discard.append(xpath)
+        self.__add(self._xpaths_to_discard, xpath)
         return self
 
     def extract(self, html_contents, css_contents=None, base_url=None, rel_to_abs=False):
         """
         Extracts the cleaned html tree as a string and only
         css rules matching the cleaned html tree
+
+        :param html_contents: The HTML contents to parse
+        :type html_contents: str
+        :param css_contents: The CSS contents to parse
+        :type css_contents: str
+        :param base_url: The base page URL to use for relative to absolute links
+        :type base_url: str
+        :param rel_to_abs: Convert relative links to absolute ones
+        :type rel_to_abs: bool
+
+        :returns: cleaned HTML contents, cleaned CSS contents
+        :rtype: str or tuple
         """
         # Clean HTML
-        cleaned_tree = self._extract_html(html_contents)
-
-        if cleaned_tree is not None:
+        if self._extract_html(html_contents):
 
             # Relative to absolute URLs
             if rel_to_abs and base_url is not None:
-                cleaned_tree = self._rel_to_abs_html(cleaned_tree, base_url)
+                self._rel_to_abs_html(base_url)
 
             # Convert ElementTree to string
-            cleaned_html = html.tostring(cleaned_tree).decode()
+            cleaned_html = html.tostring(self.tree).decode()
 
         else:
             cleaned_html = None
@@ -90,6 +112,22 @@ class TreeExtractor(object):
 
         return (cleaned_html, cleaned_css)
 
+    ##################
+    # Rules handling #
+    ##################
+
+    def __add(self, dest, e):
+        """
+        Adds a Xpath expression or HtmlElement to the dest list
+
+        :param dest: The destination list to add the element/Xpath
+        :type dest: list
+        :param e: The HtmlElement or Xpath expression to add
+        :type e: lxml.html.HtmlElement or str
+        """
+        assert isinstance(e, string_types)
+        dest.append(e)
+
     ################
     # HTML parsing #
     ################
@@ -97,6 +135,12 @@ class TreeExtractor(object):
     def _extract_html(self, html_contents):
         """
         Returns a cleaned lxml ElementTree
+
+        :param html_contents: The HTML contents to parse
+        :type html_contents: str
+
+        :returns: The cleaned lxml tree
+        :rtype: lxml.html.HtmlElement
         """
         # Create the ElementTree
         self.tree = html.fromstring(html_contents)
@@ -113,42 +157,67 @@ class TreeExtractor(object):
         has_descendant = self._has_keep_elt_in_descendants(self.tree)
 
         if not(is_root or has_descendant):
-            return None
+            return False
 
         # Parse and clean the ElementTree
-        self._parse_element(self.tree, is_keep=is_root)
+        self._parse_element(self.tree, parent_is_keep=is_root)
         self._remove_elements()
 
-        return self.tree
+        return True
+
+    def _get_elements(self, source):
+        """
+        Returns the list of HtmlElements for the source
+
+        :param source: The source list to parse
+        :type source: list
+        :returns: A list of HtmlElements
+        :rtype: list
+        """
+        return list(chain(*[self.tree.xpath(xpath) for xpath in source]))
 
     def _get_elements_to_keep(self):
         """
         Returns a list of lxml Elements to keep
+
+        :returns: List of elements to keep
+        :rtype: list of lxml.html.HtmlElement
         """
-        return list(chain(*[self.tree.xpath(x) for x in self.xpaths_to_keep]))
+        return self._get_elements(self._xpaths_to_keep)
 
     def _get_elements_to_discard(self):
         """
         Returns a list of lxml Elements to discard
-        """
-        return list(chain(*[self.tree.xpath(x) for x in self.xpaths_to_discard]))
 
-    def _parse_element(self, elt, is_keep=False):
+        :returns: List of elements to discard
+        :rtype: list of lxml.html.HtmlElement
         """
-        Parses an Element
+        return self._get_elements(self._xpaths_to_discard)
+
+    def _parse_element(self, elt, parent_is_keep=False):
+        """
+        Parses an Element recursively
+
+        :param elt: HtmlElement to parse
+        :type elt: lxml.html.HtmlElement
+        :param parent_is_keep: Whether the element is inside a keep element or not
+        :type parent_is_keep: bool
         """
         for e in elt.iterchildren():
 
+            is_discard_element = self._is_discard(e)
+            is_keep_element = self._is_keep(e)
+
             # Element is an explicit one to discard, flag it and continue
-            if self._is_discard(e) and not self._is_keep(e):
+            if is_discard_element and not is_keep_element:
                 self.elts_to_remove.append(e)
                 continue
 
-            if not is_keep:
+            if not parent_is_keep:
                 # Parent element is not an explicit keep, normal process
                 # Element is an explicit one to keep, inspect it
-                if self._is_keep(e):
-                    self._parse_element(e, is_keep=True)
+                if is_keep_element:
+                    self._parse_element(e, parent_is_keep=True)
                     continue
 
                 # Has a descendant to keep, inspect it
@@ -161,28 +230,46 @@ class TreeExtractor(object):
 
             else:
                 # Element is a child of a keep element, only check explicit discards
-                self._parse_element(e, is_keep=True)
+                self._parse_element(e, parent_is_keep=True)
 
     def _is_keep(self, elt):
         """
         Returns whether an Element is an explicit one to keep or not
+
+        :param elt: The HtmlElement to check
+        :type elt: str
+        :returns: True if the element is an explicit one to keep
+        :rtype: bool
         """
         return elt in self.elts_to_keep
 
     def _is_discard(self, elt):
         """
         Returns whether an Element is an explicit one to discard or not
+
+        :param elt: The HtmlElement to check
+        :type elt: str
+        :returns: True if the element is an explicit one to discard
+        :rtype: bool
         """
         return elt in self.elts_to_discard
 
     def _has_keep_elt_in_descendants(self, elt):
         """
         Returns whether the element has a descendant to keep or not
-        """
-        # Get Element descendants
-        descendants = list(elt.iterdescendants())
 
-        return any(k in descendants for k in self.elts_to_keep)
+        :param elt: The HtmlElement to check
+        :type elt: lxml.html.HtmlElement
+        :returns: True if the element has a keep element in its descendants
+        :rtype: bool
+        """
+        # iterdescendants is a generator, don't cast it as a list to avoid
+        # parsing the whole descendants tree if not necessary
+        for d in elt.iterdescendants():
+            if d in self.elts_to_keep:
+                return True
+
+        return False
 
     def _remove_elements(self):
         """
@@ -201,20 +288,23 @@ class TreeExtractor(object):
             # Remove the element
             e.getparent().remove(e)
 
-    def _rel_to_abs_html(self, tree, base_url):
+    def _rel_to_abs_html(self, base_url):
         """
         Converts relative links from html contents to absolute links
+
+        :param base_url: The base page url to use for building absolute links
+        :type base_url: str
         """
         # Delete target attributes
-        strip_attributes(tree, 'target')
+        strip_attributes(self.tree, 'target')
 
         # Absolute links
-        tree.rewrite_links(
+        self.tree.rewrite_links(
             lambda link: urljoin(base_url, link)
             if not link.startswith(self.rel_to_abs_html_excluded_prefixes) else link)
 
         # Extra attributes
-        onclick_elements = tree.xpath('//*[@onclick]')
+        onclick_elements = self.tree.xpath('//*[@onclick]')
 
         for element in onclick_elements:
             # Replace attribute with absolute URL
@@ -224,8 +314,6 @@ class TreeExtractor(object):
                         match.group('ending')),
                 element.get('onclick')))
 
-        return tree
-
     ###############
     # CSS Parsing #
     ###############
@@ -233,6 +321,11 @@ class TreeExtractor(object):
     def _extract_css(self, css_contents):
         """
         Returns the cleaned css only matching the ElementTree
+
+        :param css_contents: The CSS contents to parse
+        :type css_contents: str
+        :returns: The cleaned CSS contents
+        :rtype: str
         """
         # Parse the CSS contents
         stylesheet = self.css_parser.parse_stylesheet(
@@ -244,6 +337,11 @@ class TreeExtractor(object):
     def _clean_css(self, stylesheet):
         """
         Returns the cleaned CSS
+
+        :param stylesheet: The Stylesheet object to parse
+        :type stylesheet: tinycss.css21.Stylesheet
+        :returns: The cleaned CSS contents
+        :rtype: str
         """
         # Init the cleaned CSS rules and contents string
         css_rules = []
@@ -264,6 +362,11 @@ class TreeExtractor(object):
     def _rule_matches_tree(self, rule):
         """
         Returns whether the rule matches the HTML tree
+
+        :param rule: CSS Rule to check
+        :type rule: A tinycss Rule object
+        :returns: True if the rule has matches in self.tree
+        :rtype: bool
         """
         # Always return True for @ rules
         if rule.at_keyword:
@@ -275,6 +378,11 @@ class TreeExtractor(object):
     def _build_css(self, rules):
         """
         Returns a CSS string for the given rules
+
+        :param rules: List of tinycss Rule
+        :type rules: list of tinycss Rule objects
+        :returns: CSS contents for the rules
+        :rtype: string
         """
         # Build and return the cleaned CSS contents
         return '\n'.join(self._rule_as_string(rule) for rule in rules)
@@ -282,6 +390,11 @@ class TreeExtractor(object):
     def _rule_as_string(self, rule):
         """
         Converts a tinycss rule to a formatted CSS string
+
+        :param rule: The rule to format
+        :type rule: tinycss Rule object
+        :returns: The Rule as a CSS string
+        :rtype: str
         """
         if isinstance(rule, RuleSet):
             # Simple CSS rule : a { color: red; }
@@ -318,12 +431,24 @@ class TreeExtractor(object):
     def _declarations_as_string(self, declarations):
         """
         Returns a list of declarations as a formatted CSS string
+
+        :param declarations: The list of tinycss Declarations to format
+        :type declarations: list of tinycss.css21.Declaration
+        :returns: The CSS string for the declarations list
+        :rtype: str
         """
         return ''.join('%s:%s;' % (d.name, d.value.as_css()) for d in declarations)
 
     def _rel_to_abs_css(self, base_url, css_contents):
         """
         Converts relative links from css contents to absolute links
+
+        :param base_url: The base page url to use for building absolute links
+        :type base_url: str
+        :param css_contents: The CSS contents to parse
+        :type css_contents: str
+        :returns: The CSS contents with relative links converted to absolutes ones
+        :rtype: str
         """
         return self.rel_to_abs_css_re.sub(
             lambda match: "url('%s')" % urljoin(base_url, match.group('path').strip('\'"')),
